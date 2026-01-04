a4930b8e6809ed2cb087a5e05bdfca51cec18521
vanshika Bhadani <173027675+VanshikaBhadani7@users.noreply.github.com>
2026-01-04 15:17:02 +0530

    Time: 10 ms (79.55%), Space: 46.6 MB (33.08%) - LeetHub

---
+++ 1390-four-divisors/1390-four-divisors.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int factors(int n) {
+       // int m = n.length;
+        int sum = 0 ; 
+        int c =0 ;
+        for(int i = 2 ; i*i <= n; i++) {
+            if(n %i==0){
+                int j = n/i;
+                if(j==i || c > 0) return 0 ; 
+                sum += i+j;
+                c++;
+            }
+        }
+        if(c==0) return 0;
+        return 1+sum+n;
+
+
+        
+    }
+    public int sumFourDivisors(int[] nums) {
+        int sum =0 ;
+        for(int i = 0 ; i < nums.length;i++) {
+            sum += factors(nums[i]);
+
+        }
+        return sum;
+    }
+}
